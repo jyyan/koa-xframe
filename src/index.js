82763ac3037f5fd2ba4ffc4219741e0f67ee9f44
JunYuan Yan <jyyan.tw@gmail.com>
2020-10-12 15:42:02 +0800

    update to babel 7.11 & update package dependencies & add use `ALLOW-FROM uri`

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 const canBe = ['DENY', 'SAMEORIGIN'];
 
 const xFrame = (value = 'DENY') => {
-  if (!~canBe.indexOf(value)) {
-    throw new Error(`X-Frame-Options value '${value}' is not allowed. Expected 'DENY' or 'SAMEORIGIN'`);
+  if (! (~canBe.indexOf(value) || ~value.indexOf('ALLOW-FROM'))) {
+    throw new Error('X-Frame-Options value \'' + value + '\' is not allowed. Expected \'DENY\' or \'SAMEORIGIN\' or \'ALLOW-FROM uri\'');
   }
 
   return async (ctx, next) => {
     await next();
-    // headers only necessary for HTML pages
-    const {type} = ctx;
-    if (type && ~type.indexOf('text/html')) {
-      ctx.response.set('X-Frame-Options', value);
-    }
+    // headers necessary for HTML pages & All Assets
+    ctx.response.set('X-Frame-Options', value);
   }
 }
 
